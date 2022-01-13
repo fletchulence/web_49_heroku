e9917ef32a942b245a493e7e5f276d637a987efd
david fletcher <davidfletcheraaron@gmail.com>
2022-01-13 13:13:35 -0500

    about ready to deploy

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-const homebrewPre = process.env.HOMEBREW_PREFIX
+// injects the config variables inside the config file
+require('dotenv').config()
 
+// const homebrewPre = process.env.HOMEBREW_PREFIX
 
+// const cohort1 = process.argv[2]
 
-if (process.argv[2] === 'web-49'){
-   console.log('web 49 is the greatest!')
-} else {
-   console.log(process.argv)
-}
+// if ( cohort1 === 'web-49'){
+//    console.log('web 49 is the greatest!')
+// } else {
+//    console.log('process.argv')
+// }
 
-console.log(homebrewPre)
+// console.log(homebrewPre)
 
 const express = require('express')
 const app = express()
 
-const PORT = 9000 || process.env.PORT;
+const PORT = process.env.PORT || 9000;
+const secret = process.env.SECRET_THING
 
-app.get('/', (req, res)=>{
+app.get('/hello', (req, res)=>{
    res.json({ message: 'hey there' })
 })
 
 app.listen(PORT, ()=>{
-   console.log(`listening on port: ${PORT}`)
+   console.log(`listening on port: ${PORT} ${secret}`)
 })
 
